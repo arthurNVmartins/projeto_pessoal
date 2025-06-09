3b8ae63d81fcbcda3221015e3a9f2d76d5cebeed
martinss999 <arthurzim2306@gmail.com>
2025-06-09 11:56:45 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template
 
-app=Flask(__name__)
+app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html') 
-@app.route('/')
-
+@app.route('/index')
+def index():
+    return render_template('index.html')
+@app.route('/historias')
+def historias():
+    return render_template('historias.html')
 if __name__=='__main__':
     app.run(debug=True)

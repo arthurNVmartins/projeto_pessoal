f91daa1fcba154ff1095e4cfbdfc5329a72718fb
martinss999 <arthurzim2306@gmail.com>
2025-06-17 08:12:39 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html') 
+    return render_template('index.html')#vai interligar o cófigo ao site 
 @app.route('/index')
 def index():
     return render_template('index.html')
